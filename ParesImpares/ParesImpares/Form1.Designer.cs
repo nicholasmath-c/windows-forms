8669cf1ceeef0ef70e745ed76e07c85d7ee2cfbf
Nicholas M. A. Costa <59261158+Mus4ng@users.noreply.github.com>
2022-08-29 21:25:07 -0300

    Add files via upload

---
+++ ParesImpares/ParesImpares/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace ParesImpares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInicio = new System.Windows.Forms.TextBox();
+            this.txtFim = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdbPar = new System.Windows.Forms.RadioButton();
+            this.rdbImpar = new System.Windows.Forms.RadioButton();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(87, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Início:";
+            // 
+            // txtInicio
+            // 
+            this.txtInicio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInicio.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInicio.Location = new System.Drawing.Point(92, 136);
+            this.txtInicio.Multiline = true;
+            this.txtInicio.Name = "txtInicio";
+            this.txtInicio.Size = new System.Drawing.Size(180, 28);
+            this.txtInicio.TabIndex = 1;
+            // 
+            // txtFim
+            // 
+            this.txtFim.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFim.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFim.Location = new System.Drawing.Point(92, 229);
+            this.txtFim.Multiline = true;
+            this.txtFim.Name = "txtFim";
+            this.txtFim.Size = new System.Drawing.Size(180, 28);
+            this.txtFim.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(87, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fim:";
+            // 
+            // rdbPar
+            // 
+            this.rdbPar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rdbPar.AutoSize = true;
+            this.rdbPar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rdbPar.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbPar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.rdbPar.Location = new System.Drawing.Point(567, 136);
+            this.rdbPar.Name = "rdbPar";
+            this.rdbPar.Size = new System.Drawing.Size(63, 38);
+            this.rdbPar.TabIndex = 4;
+            this.rdbPar.TabStop = true;
+            this.rdbPar.Text = "Par";
+            this.rdbPar.UseVisualStyleBackColor = true;
+            // 
+            // rdbImpar
+            // 
+            this.rdbImpar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rdbImpar.AutoSize = true;
+            this.rdbImpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rdbImpar.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbImpar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.rdbImpar.Location = new System.Drawing.Point(567, 204);
+            this.rdbImpar.Name = "rdbImpar";
+            this.rdbImpar.Size = new System.Drawing.Size(90, 38);
+            this.rdbImpar.TabIndex = 5;
+            this.rdbImpar.TabStop = true;
+            this.rdbImpar.Text = "Ímpar";
+            this.rdbImpar.UseVisualStyleBackColor = true;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMostrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(248)))));
+            this.btnMostrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(248)))));
+            this.btnMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrar.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.ForeColor = System.Drawing.Color.White;
+            this.btnMostrar.Location = new System.Drawing.Point(554, 378);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(135, 40);
+            this.btnMostrar.TabIndex = 6;
+            this.btnMostrar.Text = "MOSTRAR";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(554, 464);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(135, 40);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(87, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 28);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultado:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResultado.Font = new System.Drawing.Font("Poppins", 12F);
+            this.txtResultado.Location = new System.Drawing.Point(92, 360);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultado.Size = new System.Drawing.Size(435, 182);
+            this.txtResultado.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(293, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(193, 37);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "PARES E ÍMPARES";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(3)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(763, 590);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.rdbImpar);
+            this.Controls.Add(this.rdbPar);
+            this.Controls.Add(this.txtFim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInicio);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pares e Ímpares";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInicio;
+        private System.Windows.Forms.TextBox txtFim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rdbPar;
+        private System.Windows.Forms.RadioButton rdbImpar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ParesImpares/ParesImpares/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParesImpares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            int inicio, fim;
+            string resultado = null;
+
+            if (txtInicio.Text != "" && txtFim.Text != "") 
+            {
+                if (int.TryParse(txtInicio.Text, out inicio) && int.TryParse(txtFim.Text, out fim))
+                {
+                    if (inicio >= 0 && fim >= 0)
+                    {
+                        if (inicio <= fim)
+                        {
+                            if (rdbPar.Checked)
+                            {
+                                for (int i = inicio; i <= fim; i++)
+                                {
+                                    if (i % 2 == 0)
+                                    {
+                                        if (i == inicio || i == inicio + 1)
+                                        {
+                                            resultado = i.ToString();
+                                        }
+                                        else
+                                        {
+                                            resultado += ", " + i.ToString();
+                                        }
+                                    }
+                                }
+
+                                txtResultado.Text = resultado;
+                            }
+                            else if (rdbImpar.Checked)
+                            {
+                                for (int i = inicio; i <= fim; i++)
+                                {
+                                    if (i % 2 != 0)
+                                    {
+                                        if (i == inicio || i == inicio+1)
+                                        {
+                                            resultado = i.ToString();
+                                        }
+                                        else
+                                        {
+                                            resultado += ", " + i.ToString();
+                                        }
+                                    }
+                                }
+
+                                txtResultado.Text = resultado;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Marque o campo Par ou Ímpar para exibir o resultado.");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("O início precisa possuir um valor menor que o fim.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Os valores devem ser positivos.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Insira apenas valores numéricos inteiros.");
+                    txtInicio.Clear();
+                    txtFim.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Preencha os valores de início e fim.");
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtInicio.Clear();
+            txtFim.Clear();
+            txtResultado.Clear();
+            rdbPar.Checked = false;
+            rdbImpar.Checked = false;
+        }
+    }
+}
